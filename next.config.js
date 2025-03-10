5690adae6216bd695ea2148f0e1e7315bc6ba984
mochrafi <rafisetiadipura@gmail.com>
2025-03-10 15:55:45 +0700

    update package.json

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'export',
-  distDir: 'dist',
-  trailingSlash: true,
-  assetPrefix: '.',
-}
+const nextConfig = {}
 
 export default withNextIntl(nextConfig)
